8e601be47cc89c9b6cb0094704b387666fed0c72
Christian Bobis <jonchristian.bobis.cics@ust.edu.ph>
2024-12-14 06:15:37 +0800

    maaus na shopjs

--- JS Scripts/shop.js
+++ JS Scripts/shop.js
@@ -1,11 +1,155 @@
+const shoeData = [
+  {
+    ifSale: true,
+    shoePicture: "Air Jordan 1 Low Quai 54 Off-NoirSummit White .png",
+    brandName: "Nike",
+    shoeName: "Air Jordan 1 Low Quai 54",
+    gender: "Men's",
+    shoeColor: "Summit White",
+    originalPrice: "7,895",
+    salePrice: "5,529",
+    size: [5, 7, 8, 10],
+  },
+  {
+    ifSale: false,
+    shoePicture: "Air Jordan 1 Low White Royal Blue.png",
+    brandName: "Nike",
+    shoeName: "Air Jordan 1 Low",
+    gender: "Men's",
+    shoeColor: "Alternate Royal Blue",
+    originalPrice: "7,895",
+    salePrice: "",
+    size: [6, 7.5, 9, 10.5],
+  },
+  {
+    ifSale: false,
+    shoePicture: "air jordan 1 mid se asw black white.png",
+    brandName: "Nike",
+    shoeName: "Air Jordan 1 Mid SE ASW",
+    gender: "Men's",
+    shoeColor: "Black White",
+    originalPrice: "11,385",
+    salePrice: "",
+    size: [8, 8.5, 9, 12],
+  },
+  {
+    ifSale: false,
+    shoePicture: "air jordan 1 retro black green glow wolf grey.png",
+    brandName: "Nike",
+    shoeName: "Air Jordan 1 Retro",
+    gender: "Men's",
+    shoeColor: "Black Green Glow Wolf Grey",
+    originalPrice: "9,895",
+    salePrice: "",
+    size: [4.5, 7, 7.5, 9],
+  },
+  {
+    ifSale: false,
+    shoePicture: "SailKhakiWhiteCacao Wow.png",
+    brandName: "Nike",
+    shoeName: "Cortez",
+    gender: "Women's",
+    shoeColor: "Sail Cacao Wow",
+    originalPrice: "6,500",
+    salePrice: "",
+    size: [7, 8, 9, 10.5],
+  },
+  {
+    ifSale: true,
+    shoePicture: "Nike Dunk Low Next Nature Brown Barouqe.png",
+    brandName: "Nike",
+    shoeName: "Dunk Low Next Nature",
+    gender: "Men's",
+    shoeColor: "Brown Baroque",
+    originalPrice: "7,399",
+    salePrice: "4,395",
+    size: [7, 8, 8.5, 14],
+  },
+  {
+    ifSale: false,
+    shoePicture: "nike dunk low retro white court purple.png",
+    brandName: "Nike",
+    shoeName: "Dunk Low Retro",
+    gender: "Men's",
+    shoeColor: "White Court Purple",
+    originalPrice: "12,000",
+    salePrice: "",
+    size: [8, 9, 10, 10.5],
+  },
+  {
+    ifSale: true,
+    shoePicture: "Women Nike Dunk Low Retro Gorge Green Midnight Navy(1).png",
+    brandName: "Nike",
+    shoeName: "Dunk Low Retro",
+    gender: "Women's",
+    shoeColor: "Gorge Green Midnight Navy",
+    originalPrice: "10,299",
+    salePrice: "7,399",
+    size: [7.5, 8, 9, 11],
+  },
+  {
+    ifSale: true,
+    shoePicture: "Women Nike Dunk Low Rose Whisper.png",
+    brandName: "Nike",
+    shoeName: "Dunk Low",
+    gender: "Women's",
+    shoeColor: "Rose Whisper",
+    originalPrice: "10,799",
+    salePrice: "7,399",
+    size: [11, 12, 13, 15],
+  },
+  {
+    ifSale: false,
+    shoePicture: "nike dunk low sp university red.png",
+    brandName: "Nike",
+    shoeName: "Dunk Low SP",
+    gender: "Men's",
+    shoeColor: "White and University Red",
+    originalPrice: "5,495",
+    salePrice: "",
+    size: [6, 7, 8, 9],
+  },
+  {
+    ifSale: false,
+    shoePicture: "Nike Mens Air Zoom Gt Cut Academy Ep.png",
+    brandName: "Nike",
+    shoeName: "G.T. Cut Academy",
+    gender: "Men's",
+    shoeColor: "White Sport Red Obsidian",
+    originalPrice: "4,995",
+    salePrice: "",
+    size: [4, 8, 9, 10],
+  },
+  {
+    ifSale: false,
+    shoePicture: "new balance mr530ka steel grey.png",
+    brandName: "New Balance",
+    shoeName: "530",
+    gender: "Unisex",
+    shoeColor: "Steel Grey",
+    originalPrice: "6,295",
+    salePrice: "",
+    size: [9, 10, 11, 13],
+  },
+  {
+    ifSale: false,
+    shoePicture: "Onitsuka Tiger Tokuten White Moss Green (1).png",
+    brandName: "Onitsuka Tiger",
+    shoeName: "Tokuten",
+    gender: "Men's",
+    shoeColor: "Sail Cacao Wow",
+    originalPrice: "10,799",
+    salePrice: "",
+    size: [8.5, 9, 10, 11.5],
+  },
+];
+
 function setActive(button) {
-  // Remove active class from all buttons
   const buttons = document.querySelectorAll(".div-wrapper");
   buttons.forEach((btn) => {
     btn.classList.remove("active");
   });
 
-  // Add active class to the clicked button
   button.classList.add("active");
 }
 
@@ -14,11 +158,13 @@ const cardsContainer = document.querySelector(".items-frame");
 // let shoeData = [ifSale, shoePicture, brandName, shoeName, gender, shoeColor, originalPrice, salePrice, size];
 let shoeCards = [];
 
-function createCard(ifSale, shoePicture, brandName, shoeName, gender, shoeColor, originalPrice, salePrice, size) {
-  const card = document.createElement("div");
-  card.setAttribute("class", "card-desktop");
+function display() {
+  cardsContainer.innerHTML = "";
+  for (shoe in shoeData) {
+    const card = document.createElement("div");
+    card.setAttribute("class", "card-desktop");
 
-  card.innerHTML = `
+    card.innerHTML = `
     <div class='top-group'>
       <div class='picture-and-sale-frame'>
       ${ifSale ? `<div class='sale-frame'><div class='sale-text'>SALE</div></div>` : ""}
@@ -45,43 +191,19 @@ function createCard(ifSale, shoePicture, brandName, shoeName, gender, shoeColor,
       </div>
     </div>
   `;
-
-  shoeCards.push({
-    ifSale,
-    shoePicture,
-    brandName,
-    shoeName,
-    gender,
-    shoeColor,
-    originalPrice,
-    salePrice,
-    size,
-  });
-
-  const likeButton = card.querySelector(".like-button");
-  likeButton.addEventListener("click", () => {
-    if (likeButton.src.includes("/YSC/folder-icons/heart.svg")) {
-      likeButton.src = "/YSC/folder-icons/heart-fill.svg";
-    } else {
-      likeButton.src = "/YSC/folder-icons/heart.svg";
-    }
-  });
-
-  cardsContainer.appendChild(card);
+    cardsContainer.appendChild(card);
+  }
 }
 
-createCard(true, "Air Jordan 1 Low Quai 54 Off-NoirSummit White .png", "Nike", "Air Jordan 1 Low Quai 54", "Men's", "Summit White", "7,895", "5,529", [5, 7, 8, 10]);
-createCard(false, "Air Jordan 1 Low White Royal Blue.png", "Nike", "Air Jordan 1 Low", "Men's", "Alternate Royal Blue", "7,895", "", [6, 7.5, 9, 10.5]);
-createCard(false, "air jordan 1 mid se asw black white.png", "Nike", "Air Jordan 1 Mid SE ASW", "Men's", "Black White", "11,385", "", [8, 8.5, 9, 12]);
-createCard(false, "air jordan 1 retro black green glow wolf grey.png", "Nike", "Air Jordan 1 Retro", "Men's", "Black Green Glow Wolf Grey", "9,895", "", [4.5, 7, 7.5, 9]);
-createCard(false, "SailKhakiWhiteCacao Wow.png", "Nike", "Cortez", "Women's", "Sail Cacao Wow", "6,500", "", [7, 8, 9, 10.5]);
-createCard(true, "Nike Dunk Low Next Nature Brown Barouqe.png", "Nike", "Dunk Low Next Nature", "Men's", "Brown Baroque", "7,399", "4,395", [7, 8, 8.5, 14]);
-createCard(false, "nike dunk low retro white court purple.png", "Nike", "Dunk Low Retro", "Men's", "White Court Purple", "12,000", "", [8, 9, 10, 10.5]);
-createCard(true, "Women Nike Dunk Low Retro Gorge Green Midnight Navy(1).png", "Nike", "Dunk Low Retro", "Women's", "Gorge Green Midnight Navy", "10,299", "7,399", [7.5, 8, 9, 11]);
-createCard(true, "Women Nike Dunk Low Rose Whisper.png", "Nike", "Dunk Low", "Women's", "Rose Whisper", "10,799", "7,399", [11, 12, 13, 15]);
-createCard(false, "nike dunk low sp university red.png", "Nike", "Dunk Low SP", "Men's", "White and University Red", "5,495", "", [6, 7, 8, 9]);
-createCard(false, "Nike Mens Air Zoom Gt Cut Academy Ep.png", "Nike", "G.T. Cut Academy", "Men's", "White Sport Red Obsidian", "4,995", "", [4, 8, 9, 10]);
-createCard(false, "new balance mr530ka steel grey.png", "New Balance", "530", "Unisex", "Steel Grey", "6,295", "", [9, 10, 11, 13]);
-createCard(false, "Onitsuka Tiger Tokuten White Moss Green (1).png", "Onitsuka Tiger", "Tokuten", "Men's", "Sail Cacao Wow", "10,799", "", [8.5, 9, 10, 11.5]);
+const likeButton = card.querySelector(".like-button");
+likeButton.addEventListener("click", () => {
+  if (likeButton.src.includes("/YSC/folder-icons/heart.svg")) {
+    likeButton.src = "/YSC/folder-icons/heart-fill.svg";
+  } else {
+    likeButton.src = "/YSC/folder-icons/heart.svg";
+  }
+});
+
+cardsContainer.appendChild(card);
 
 console.log(shoeCards);
